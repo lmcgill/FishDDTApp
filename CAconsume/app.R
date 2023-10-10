686f236e90430f99e2a104cfdf62fbfb49d656f4
Lillian McGill <lmcgill@uw.edu>
2023-10-10 14:45:43 -0700

    OEHHA consumption advisory posters included in data.

--- CAconsume/app.R
+++ CAconsume/app.R
@@ -23,12 +23,16 @@ ui <- bootstrapPage(
              windowTitle = "CA Fish Consumption (in development)",
              
              tabPanel("Background"),
-             tabPanel("What Did I Catch?"),
+             tabPanel("What Did I Catch?", 
+                      mainPanel(
+                        tags$h2(""),
+                        
+                      )),
              tabPanel("My Catch", 
                       sidebarLayout(
                         sidebarPanel(
                           id = "sidebar",
-                          tags$h1("Modify Data Inputs"),
+                          tags$h1("Data Inputs"),
                           tags$br(),
                           HTML(paste0(
                             "<p>A side panel where users can select the correct inputs for their specific catch. To obtain accurate model predictions, users will need to:</p>",
@@ -39,11 +43,13 @@ ui <- bootstrapPage(
                         ), 
                         mainPanel(
                           tags$h2("Model Predictions for DDT"),
-                          "A plot comparing the distribution of predicted DDT concentration from the best-fit model for the specific fish-location combination
-                                  to Advisory Tissue Levels (ATLs) for the specificied demographic group. The recommended servings per week should be readily apparent.", 
+                          "This section should show predicted DDT concentrations from the best-fit model for the specific species-location combination input by the user. 
+                          Results should be compared to Advisory Tissue Levels (ATLs) for the specificied demographic group. The recommended servings per week should be readily apparent.", 
                           tags$h2("CA Consumption Recommendations"), 
-                          "Below model predictions for DDT, there should be a figure or text box that pops up with CA Consumption advice (so encompassing all 
-                          contaminants not just DDT) from OEHAA recommendations."
+                          "Below model predictions for DDT, there should be a figure or text box that pops up with offical CA Consumption advice for the species-location combination input by the user. 
+                          The 'Model Predictions for DDT' section above only examines DDT, whereas official CA consumption 
+                          advisories encompass a variety of contamiants, including DDT, Mercury, and PCBs. As DDT levels from a region may be within safe to eat thresholds, but other contamiannts may be high, 
+                          we want to give the offical OEHAA recommendations here as well."
                         ))), 
                       
   ))
