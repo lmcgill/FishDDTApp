713a3dd28bc10a7d24351cf958a8c0c3cef6f478
Lillian McGill <lmcgill@uw.edu>
2023-10-17 09:56:22 -0700

    Updates to the Rshiny App

--- CAconsume/app.R
+++ CAconsume/app.R
@@ -22,17 +22,32 @@ ui <- bootstrapPage(
              # Specify titles and that we want multiple panels 
              windowTitle = "CA Fish Consumption (in development)",
              
-             tabPanel("Background"),
-             tabPanel("What Did I Catch?", 
-                      mainPanel(
-                        tags$h2(""),
-                        
-                      )),
+             tabPanel("Background", 
+                      sidebarLayout(
+                        sidebarPanel(
+                          id = "sidebar",
+                          tags$h1("A landing page for users"),
+                          tags$br(),
+                          HTML(paste0(
+                            "<p>This page should give background information on DDT dumping in Southern California.</p>"
+                          )),
+                        ), 
+                        mainPanel(
+                          tags$h3("Possible additions to this page could include the following sections:"),
+                          tags$h2("What did I catch?"),
+                          "Show images of fish species included in the modeling effort. With over 60 species of fish and elasmobranch 
+                          included in this analysis, it would be beneficial to have a reference page
+                          where users can look at images of the species included to compare against their catch. A collection of beautiful, anatomically correct 
+                          illustrations is available here: https://marinespecies.wildlife.ca.gov/.",
+                          tags$h2("Where am I located?"), 
+                          "An interactive map, perhaps generated with leaflet, where users can click on their geographic location to get latitude and longitude 
+                          values to be used as input in the 'User Inputs - Select the location of capture' section on the My Catch page."
+                        ))), 
              tabPanel("My Catch", 
                       sidebarLayout(
                         sidebarPanel(
                           id = "sidebar",
-                          tags$h1("Data Inputs"),
+                          tags$h1("User Inputs"),
                           tags$br(),
                           HTML(paste0(
                             "<p>A side panel where users can select the correct inputs for their specific catch. To obtain accurate model predictions, users will need to:</p>",
